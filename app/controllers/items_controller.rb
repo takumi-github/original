2b9aeeda28ffd692a1494197eb87fe3bf9787c60
takumi.naitou <hello7110baseball7@icloud.com>
2020-09-06 07:42:29 +0000

    original（改2）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:edit, :destroy]
   
   def index
     @items = Item.order(id: :desc).page(params[:page])
